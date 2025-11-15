95d39f45437ed27e4ae5f3293981c24217434cd1
DiptanshuSin <singhdiptanshu08@gmail.com>
2025-11-16 01:44:22 +0530

    Update dummyTest.java

--- src/test/java/com/OrangeHRM/test/dummyTest.java
+++ src/test/java/com/OrangeHRM/test/dummyTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 import com.OrangeHRM.base.BaseClass;
 
 public class dummyTest extends BaseClass {
-	
+
 	@Test
 	public void dummyTestfirst() {
 		String title = driver.getTitle();
-		assert title.equals("OrangeHRM"): "Test Failed- Title does not Match";
-		
+		assert title.equals("OrangeHRM") : "Test Failed- Title does not Match";
+
 		System.out.println("Test Passed- Title matching");
 	}
 
